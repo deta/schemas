fab00ca37a8b456886065d1cd851dcd2b2958b09
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-04-05 04:21:10 -0400

    Add testing framework (#4)
    * Fix schema to conform to draft-04 spec
    
    * Add testing framework
    
    * Upgrade spec to draft-07
    
    * Add vscode settings
    
    * Update code for draft-07
    
    * Change test structure
    
    * Add schema for tests definition file
    
    * Add comment to tests schema
    
    * Update README
    
    * Remove ajv-draft-04 from package-lock
    
    * Add clearer validated/invalidated console output
    
    * Refactor and format
    
    * Add verbose command to README
    
    * Remove json.schemas from settings.json
    
    * Add self-test for tests.yaml file
    
    * Fix tests schema
    
    * Add validation errors to output
    
    * Add more tests
    
    * Prettier errors output formatting
    
    * Fix no primary micro test
    
    * Disable duplicate micros test
    
    * Add serve property tests
    
    * Use local schema for Spacefiles
    
    * Better verbose output
    
    * Add uniqueItems to array properties
    
    * Add public property
    
    * Make serve property conditional
    
    * Format with Prettier
    
    * Disable primary property tests
    
    * Remove unused uniqueItems attributes
    
    * Fix serve being required for Micros without engine
    
    * Move test definitions from YAML to TS
    
    * Remove yaml dependency
    
    * Goodbye beloved self-test schema
    
    * Add exit code
    
    * Add github workflow
    
    * Update README
    
    * Fix README
    
    * Make description property required

---
+++ tests/tests.ts
@@ -0,0 +1,211 @@
+export type Test = {
+  name: string;
+  description: string;
+  valid: boolean;
+  schema: string;
+  version: number;
+  data: Object;
+};
+
+export const tests: Test[] = [
+  {
+    name: "Single Micro",
+    description: "Tests a Spacefile configuration with a single Micro",
+    valid: true,
+    schema: "spacefile",
+    version: 0,
+    data: {
+      v: 0,
+      micros: [
+        {
+          name: "go-app",
+          src: ".",
+          engine: "custom",
+          primary: true,
+          commands: ["go get", "go build main.go"],
+          include: ["main", "static"],
+          run: "./main",
+        },
+      ],
+    },
+  },
+  {
+    name: "Multiple Micros",
+    description: "Tests a Spacefile configuration with multiple Micros",
+    valid: true,
+    schema: "spacefile",
+    version: 0,
+    data: {
+      v: 0,
+      micros: [
+        {
+          name: "go-app",
+          src: ".",
+          engine: "custom",
+          commands: ["go get", "go build main.go"],
+          include: ["main", "static"],
+          run: "./main",
+        },
+        {
+          name: "python-app",
+          src: ".",
+          engine: "python3.9",
+          primary: true,
+        },
+      ],
+    },
+  },
+  {
+    name: "Invalid version",
+    description: "Tests invalid value for 'v' attribute",
+    valid: false,
+    schema: "spacefile",
+    version: 0,
+    data: {
+      v: 1,
+      micros: [
+        {
+          name: "micro-name",
+          src: ".",
+          engine: "custom",
+          primary: true,
+          path: "micro/url",
+          run: "executable",
+        },
+      ],
+    },
+  },
+  {
+    name: "Serve property",
+    description: "Tests a configuration with a static Micro and a 'serve' property",
+    valid: true,
+    schema: "spacefile",
+    version: 0,
+    data: {
+      v: 0,
+      micros: [
+        {
+          name: "static-micro",
+          src: ".",
+          engine: "static",
+          serve: "dist",
+        },
+      ],
+    },
+  },
+  {
+    name: "Missing serve property",
+    description: "Tests an invalid configuration with a static Micro and no 'serve' property",
+    valid: false,
+    schema: "spacefile",
+    version: 0,
+    data: {
+      v: 0,
+      micros: [
+        {
+          name: "static-micro",
+          src: ".",
+          engine: "static",
+        },
+      ],
+    },
+  },
+  {
+    name: "Unexpected serve property",
+    description: "Tests an invalid configuration with a non-static Micro and a 'serve' property",
+    valid: false,
+    schema: "spacefile",
+    version: 0,
+    data: {
+      v: 0,
+      micros: [
+        {
+          name: "python-app",
+          src: ".",
+          engine: "python3.9",
+          serve: "dist",
+        },
+      ],
+    },
+  },
+
+  // This test is currently not possible to validate because JSON Schema does not support unique item properties yet.
+  // https://github.com/json-schema-org/json-schema-vocabularies/issues/22
+
+  // {
+  //   name: "Duplicate micros",
+  //   description: "Tests an invalid configuration with duplicate Micro names",
+  //   valid: false,
+  //   schema: "spacefile",
+  //   version: 0,
+  //   data: {
+  //     v: 0,
+  //     micros: [
+  //       {
+  //         name: "go-app",
+  //         src: ".",
+  //         engine: "custom",
+  //         commands: ["go get", "go build main.go"],
+  //         include: ["main", "static"],
+  //         run: "./main",
+  //       },
+  //       {
+  //         name: "go-app",
+  //         src: ".",
+  //         engine: "python3.9",
+  //         primary: true,
+  //       },
+  //     ],
+  //   },
+  // },
+
+  // Tests for the 'primary' property are currently not possible to validate.
+
+  // {
+  //   name: "Multiple primary Micros",
+  //   description: "Tests an invalid configuration with multiple primary Micros",
+  //   valid: false,
+  //   schema: "spacefile",
+  //   version: 0,
+  //   data: {
+  //     v: 0,
+  //     micros: [
+  //       {
+  //         name: "micro-1",
+  //         src: ".",
+  //         engine: "python3.9",
+  //         primary: true,
+  //       },
+  //       {
+  //         name: "micro-2",
+  //         src: ".",
+  //         engine: "python3.9",
+  //         primary: true,
+  //       },
+  //     ],
+  //   },
+  // },
+
+  // {
+  //   name: "No primary Micro",
+  //   description: "Tests an invalid configuration with multiple Micros and no primary Micro",
+  //   valid: false,
+  //   schema: "spacefile",
+  //   version: 0,
+  //   data: {
+  //     v: 0,
+  //     micros: [
+  //       {
+  //         name: "micro-1",
+  //         src: ".",
+  //         engine: "python3.9",
+  //       },
+  //       {
+  //         name: "micro-2",
+  //         src: ".",
+  //         engine: "python3.9",
+  //       },
+  //     ],
+  //   },
+  // },
+];
