5586e6f64cc90f18d8852bdbb272c298de307d23
pomdtr <achille@deta.sh>
2023-04-05 11:10:36 +0200

    restructure package to have package.json at the root

--- tests/index.ts
+++ tests/index.ts
@@ -3,7 +3,8 @@ import Ajv from "ajv";
 import { tests } from "./tests";
 
 const ajv = new Ajv();
-const verbose = process.argv.includes("--verbose") || process.argv.includes("-v");
+const verbose =
+  process.argv.includes("--verbose") || process.argv.includes("-v");
 
 async function getValidator(schemaName: string, version: number) {
   let validator = ajv.getSchema(`${schemaName}.v${version}`);
@@ -11,7 +12,10 @@ async function getValidator(schemaName: string, version: number) {
     let schema;
     try {
       schema = JSON.parse(
-        await fs.readFile(`../schemas/${schemaName}/${schemaName}.v${version}.schema.json`, { encoding: "utf-8" }),
+        await fs.readFile(
+          `./schemas/${schemaName}/${schemaName}.v${version}.schema.json`,
+          { encoding: "utf-8" }
+        )
       );
     } catch (error) {
       if (verbose) {
@@ -30,7 +34,9 @@ async function runTests() {
   for (const test of tests) {
     const validator = await getValidator(test.schema, test.version);
     if (!validator) {
-      console.error(`❌ Failed to load schema "${test.schema}" version ${test.version}`);
+      console.error(
+        `❌ Failed to load schema "${test.schema}" version ${test.version}`
+      );
       passed = false;
       continue;
     }
@@ -40,12 +46,19 @@ async function runTests() {
         `\tValid: ${test.valid}\n` +
         `\tSchema: ${test.schema}\n` +
         `\tVersion: ${test.version}\n` +
-        `\tErrors: ${validator.errors?.map((error) => `\n\t\t${error.instancePath}: ${error.message}`).join("")}\n`
+        `\tErrors: ${validator.errors
+          ?.map((error) => `\n\t\t${error.instancePath}: ${error.message}`)
+          .join("")}\n`
       : "";
     if (valid === test.valid) {
-      console.log(`✅ ${test.valid ? "V" : "Inv"}alidated "${test.name}"` + details);
+      console.log(
+        `✅ ${test.valid ? "V" : "Inv"}alidated "${test.name}"` + details
+      );
     } else {
-      console.error(`❌ Failed to ${test.valid ? "v" : "inv"}alidate "${test.name}"` + details);
+      console.error(
+        `❌ Failed to ${test.valid ? "v" : "inv"}alidate "${test.name}"` +
+          details
+      );
       passed = false;
     }
   }
